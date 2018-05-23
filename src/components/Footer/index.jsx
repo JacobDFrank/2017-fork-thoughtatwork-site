b7c902dac6a2aa8c70937b91408065dca03b7885
Andrew Hurley <hurleymandrew@gmail.com>
2018-05-22 22:00:10 -0400

    npm installed back to top button

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,6 +3,7 @@ import Sponsor from "./Sponsor.jsx";
 import FooterBottom from "./FooterBottom.jsx";
 import BeSponsor from "./BeSponsor.jsx";
 import data from "../../assets/sponsors.json";
+import ScrollToTop from 'react-scroll-up';
 
 class Footer extends React.Component {
     createSponsor = sponsor => {
@@ -23,12 +24,24 @@ class Footer extends React.Component {
     render() {
         return (
             <div className="footer">
+				<ScrollToTop showUnder={160}>
+					<div id="return-to-top" className="toTop">
+					    <hr className="toTop-hr" />
+					    <a className="toTop-link">
+					        <i className="fa fa-long-arrow-up fa-lg toTop-link-icon" aria-hidden="true"></i>
+					        <br/>
+					        TOP
+					    </a>
+					    <hr className="toTop-hr" />
+					</div>
+
+				</ScrollToTop>
                 <div className="footer-sponsors">
                     {this.createSponsors(data.sponsor)}
                     <BeSponsor />
                 </div>
                 <FooterBottom />
-            </div>    
+            </div>
         );
     }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,16 @@ import Link from 'gatsby-link'
 import Img from "gatsby-image";
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/speakers/">Go to page 2</Link>
-    <Link to="/contact/">Go to page 2</Link>
-  </div>
+	<div>
+		<h1>Hi people</h1>
+		<p>Welcome to your new Gatsby site.</p>
+		<p>Now go build something great.</p>
+		<Link to="/speakers/">Go to page 2</Link>
+		<Link to="/contact/">Go to page 2</Link>
+
+
+
+	</div>
 )
 
 export default IndexPage
