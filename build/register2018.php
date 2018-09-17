ed9833de7b130b4ae0ac944853262c6bcc90d1a3
JacobDFrank <frankjacob42@gmail.com>
2018-09-17 02:01:01 -0400

    finished register page

---
+++ build/register2018.php
@@ -0,0 +1,291 @@
+<!--source /templates/layout.njk -->
+<!doctype html>
+<html>
+<head>
+	
+	
+
+	
+
+	<meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Thought At Work 2018</title>
+  <meta name="keywords" content="sample, something">
+  <meta name="format-detection" content="telephone=no">
+  <meta charset="utf-8">
+  <meta name="viewport" content="user-scalable=0, initial-scale=1.0, width=device-width, maximum-scale=1, minimum-scale=1"
+  >
+  <meta name="format-detection" content="telephone=no">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta property="og:url" content="http://thoughtatwork.cias.rit.edu">
+  <meta property="og:type" content="article">
+  <meta property="og:title" content="Thought At Work, a student-run design conference">
+  <meta property="og:description" content="Thought At Work is a three-day, student-run, student-focused design conference that takes place every October at Rochester Institute of Technology."
+  >
+  <meta property="og:image" content="http://thoughtatwork.cias.rit.edu/assets/graphics/WebBanner_TAW2018.jpg"
+  >
+  <meta property="fb:app_id" content="486507185043060">
+  <meta name="twitter:card" content="product">
+  <meta name="twitter:site" content="@TAW_RIT">
+  <meta name="twitter:title" content="Thought At Work, a student-run design conference">
+  <meta name="twitter:description" content="Thought At Work is a three-day, student-run, student-focused design conference that takes place every October at Rochester Institute of Technology."
+  >
+  <meta name="twitter:creator" content="@TAW_RIT">
+  <meta name="twitter:image" content="http://thoughtatwork.cias.rit.edu/assets/graphics/WebBanner_TAW2018.jpg"
+  >
+  <meta name="description" content="Student-Run Design Conference">
+  <meta name="title" content="Thought at Work">
+	<link rel="icon" type="image/ico" href="assets/graphics/faviconPurple2018.png">
+	<base href="/">
+	
+	<link rel="stylesheet"
+	      type="text/css"
+	      href="styles/register.css">
+	<script type="text/javascript"
+	        src="vendors/jquery.min.js"></script>
+</head>
+
+<body class="dotGrid-background">
+	
+
+    
+    
+
+    
+
+    
+
+    <div class="container register2018-container">
+        <div class="gridish-container--complete gridish-grid">
+    <div class='flex flex-align-items-start register2018-ticket register2018-ticket--border register2018-ticket--spacing'>
+
+        <img class="register2018-ticket--icon" src="../assets/graphics/icons/register2018-ticket.svg" />
+        <div class="flex flex-column register2018-ticket--content">
+            <h2 class="register2018-ticket--content-title">$40 per person</h2>
+                <p class="register2018-ticket--content-subhead__small">Participate in invaluable workshops, cool events, tasty meals, insightful talks, parties, after-parties
+                  and countless
+                  opportunities to network.</p>
+                  <p class="register2018-ticket--content-subhead__xsmall">Thought At Work 2018 Ticket</p>
+        </div>
+    </div>
+    <h3 class="content-Block--margin-top gridish-grid__col--small--4 gridish-grid__col--xsmall--3">Register now to recieve a general
+      admission
+      ticket</h3>
+</div>
+
+        <script type="text/javascript" src="vendors/jquery.min.js"></script>
+
+
+<div>
+
+    <form class="container form__margin-top gridish-container--complete gridish-grid" id="registerForm" method="post"
+        action="review.php">
+
+
+        <!-- <div class="input-field grid__col grid__col--1-of-2 grid__col--m-1-of-2">
+            <input class="form__input input-text" type="text" id="first-name" name="first-name" placeholder="First Name"
+                required />
+        </div> -->
+
+        <label class='flex-align-items-baseline flex-justify-between form-element__margin-bottom gridish-grid'>
+            <p class='form-element__label gridish-grid__col--small--2 gridish-grid__col--xsmall--4'>
+                <strong>
+                    First Name
+                    <span class='form-element__label__red'> *</span>
+                </strong>
+            </p>
+
+            <div class='form-element__margin-top form-text_input__field gridish-grid__col--small--5 gridish-grid__col--xsmall--4'>
+                <input type="text" id="first-name" name="first-name" placeholder="First Name" required class='form-text_input__text' />
+            </div>
+        </label>
+
+        <label class='flex-align-items-baseline flex-justify-between form-element__margin-bottom gridish-grid'>
+            <p class='form-element__label gridish-grid__col--small--2 gridish-grid__col--xsmall--4'>
+                <strong>
+                    Last Name
+                    <span class='form-element__label__red'> *</span>
+                </strong>
+            </p>
+
+            <div class='form-element__margin-top form-text_input__field gridish-grid__col--small--5 gridish-grid__col--xsmall--4'>
+                <input type="text" id="last-name" name="last-name" placeholder="Last Name" required class='form-text_input__text' />
+            </div>
+        </label>
+
+        <label class='flex-align-items-baseline flex-justify-between form-element__margin-bottom gridish-grid'>
+            <p class='form-element__label gridish-grid__col--small--2 gridish-grid__col--xsmall--4'>
+                <strong>
+                    Email
+                    <span class='form-element__label__red'> *</span>
+                </strong>
+            </p>
+
+            <div class='form-element__margin-top form-text_input__field gridish-grid__col--small--5 gridish-grid__col--xsmall--4'>
+                <input type="email" id="email" name="email" placeholder="Email" required class='form-text_input__text' />
+            </div>
+        </label>
+
+        <label class='flex-align-items-baseline flex-justify-between form-element__margin-bottom gridish-grid'>
+            <p class='form-element__label gridish-grid__col--small--2 gridish-grid__col--xsmall--4'>
+                <strong>
+                    Phone Number
+                    <span class='form-element__label__red'> *</span>
+                </strong>
+            </p>
+
+            <div class='form-element__margin-top form-text_input__field gridish-grid__col--small--5 gridish-grid__col--xsmall--4'>
+                <input type="tel" id="phone" name="phone" placeholder="800-856-9123" required class='form-text_input__text' />
+            </div>
+        </label>
+
+        <label class='flex-align-items-baseline flex-justify-between form-element__margin-bottom gridish-grid'>
+            <p class='form-element__label gridish-grid__col--small--2 gridish-grid__col--xsmall--4'>
+                <strong>
+                    University
+                </strong>
+            </p>
+
+            <div class='form-element__margin-top form-text_input__field gridish-grid__col--small--5 gridish-grid__col--xsmall--4'>
+                <input type="text" name="university" placeholder="RIT" class='form-text_input__text' />
+            </div>
+        </label>
+
+        <label class='flex-align-items-baseline flex-justify-between form-element__margin-bottom gridish-grid'>
+            <p class='form-element__label gridish-grid__col--small--2 gridish-grid__col--xsmall--4'>
+                <strong>
+                    Position
+                </strong>
+            </p>
+
+            <div class='form-element__margin-top form-text_input__field gridish-grid__col--small--5 gridish-grid__col--xsmall--4'>
+                <input type="text" name="position" placeholder="Major or job title" class='form-text_input__text' />
+            </div>
+        </label>
+
+        <label class='flex-align-items-baseline flex-justify-between form-element__margin-bottom gridish-grid'>
+            <p class='form-element__label gridish-grid__col--small--2 gridish-grid__col--xsmall--4'>
+                <strong>
+                    Address
+                </strong>
+            </p>
+
+            <div class='form-element__margin-top form-text_input__field gridish-grid__col--small--5 gridish-grid__col--xsmall--4'>
+                <input type="text" name="address" placeholder="Address" class='form-text_input__text' />
+            </div>
+        </label>
+
+        <label class='flex-align-items-baseline flex-justify-between form-element__margin-bottom gridish-grid'>
+            <p class='form-element__label gridish-grid__col--small--2 gridish-grid__col--xsmall--4'>
+                <strong>
+                    City
+                </strong>
+            </p>
+
+            <div class='form-element__margin-top form-text_input__field gridish-grid__col--small--5 gridish-grid__col--xsmall--4'>
+                <input type="text" name="city" placeholder="City" class='form-text_input__text' />
+            </div>
+        </label>
+
+        <label class='flex-align-items-baseline flex-justify-between form-element__margin-bottom gridish-grid'>
+            <p class='form-element__label gridish-grid__col--small--2 gridish-grid__col--xsmall--4'>
+                <strong>
+                    State
+                </strong>
+            </p>
+
+            <div class='form-element__margin-top form-text_input__field gridish-grid__col--small--5 gridish-grid__col--xsmall--4'>
+                <input type="text" name="state" placeholder="State" class='form-text_input__text' />
+            </div>
+        </label>
+
+        <label class='flex-align-items-baseline flex-justify-between form-element__margin-bottom gridish-grid'>
+            <p class='form-element__label gridish-grid__col--small--2 gridish-grid__col--xsmall--4'>
+                <strong>
+                    Zip Code
+                </strong>
+            </p>
+
+            <div class='form-element__margin-top form-text_input__field gridish-grid__col--small--5 gridish-grid__col--xsmall--4'>
+                <input type="text" name="zip" placeholder="Zip" class='form-text_input__text' />
+            </div>
+        </label>
+
+        <label class='flex-align-items-baseline flex-justify-between form-element__margin-bottom gridish-grid'>
+            <p class='form-element__label gridish-grid__col--small--2 gridish-grid__col--xsmall--4'>
+                <strong>
+                    I need interpreting services
+                </strong>
+            </p>
+
+            <div class='form-element__margin-top form-text_input__checkbox gridish-grid__col--small--1 gridish-grid__col--xsmall--1'>
+                <input value="yes" type="checkbox" name="interpret" id="yesbox" class='form-text_input__text' />
+            </div>
+        </label>
+
+        <label class='flex-align-items-baseline flex-justify-between form-element__margin-bottom gridish-grid'>
+            <p class='form-element__label gridish-grid__col--small--2 gridish-grid__col--xsmall--4'>
+                <strong>
+                    Dietary restrictions
+                </strong>
+            </p>
+
+            <div class='form-element__margin-top form-text_input__field gridish-grid__col--small--5 gridish-grid__col--xsmall--4'>
+                <input type="text" id="dietary" name="dietary" placeholder="Dietary restrictions" class='form-text_input__text' />
+            </div>
+        </label>
+
+        <label class='flex-align-items-baseline flex-justify-between form-element__margin-bottom gridish-grid'>
+            <p class='form-element__label gridish-grid__col--small--2 gridish-grid__col--xsmall--4'>
+                <strong>
+                    Have a discount code?
+                </strong>
+            </p>
+
+            <div class='form-element__margin-top form-text_input__field gridish-grid__col--small--5 gridish-grid__col--xsmall--4'>
+                <input class="form__input input-text form-text_input__text" form-text_input__text type="text" id="coupon"
+                    name="coupon" placeholder="Have a discount code?" />
+                <div class="coupon-container">
+                    <div class="coupon-status coupon-success">
+                        Valid Coupon
+                    </div>
+                    <div class="coupon-status coupon-fail">
+                        Invalid Coupon
+                    </div>
+                </div>
+            </div>
+        </label>
+
+        <!-- <div class="input-field grid__col grid__col--1-of-2 grid__col--m-1-of-2">
+            <input class="form__input input-text" type="text" id="coupon" name="coupon" placeholder="Have a discount code?" />
+            <div class="coupon-container">
+                <div class="coupon-status coupon-success">
+                    Valid Coupon
+                </div>
+                <div class="coupon-status coupon-fail">
+                    Invalid Coupon
+                </div>
+            </div>
+        </div> -->
+        <div class="gridish-container--complete form-actionButton">
+            <button type="submit" value="SUBMIT" class="form-actionButton__text">
+                submit
+            </button>
+        </div>
+        <!-- <div class="text-align-right">
+            <button class="flex-inline flex-align-items-center flex-justify-center action--link--button submit-link">
+                <input type="submit" value="SUBMIT" class="action--link--button grid__col grid__col--m-1-of-5" />
+                <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+            </button>
+        </div> -->
+    </form>
+</div>
+
+        
+    </div>
+
+	<!-- Load scripts. -->
+</body>
+</html>
